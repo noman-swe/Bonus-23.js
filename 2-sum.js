3ab7b93249c63556e3e586b92c360de69a840473
noman-swe <noman.swe130@gmail.com>
2022-02-06 22:27:54 +0600

    23_5-2 Understand recursion using sum of numbers

---
+++ 2-sum.js
@@ -0,0 +1,21 @@
+ /*
+let sum = 0;
+for(let i = 5; i >= 1; i--){
+    sum = sum + i;   
+}
+console.log(sum);
+*/ 
+/*  uporer function er summary
+i = 5 asy
+i er value 1 er theke boro hoile i--
+sum = sum - i;
+*/
+
+function sum(i){
+    console.log(i);
+    if(i == 1){
+        return 1;
+    }
+    return sum(--i);
+}
+console.log(sum(5));
